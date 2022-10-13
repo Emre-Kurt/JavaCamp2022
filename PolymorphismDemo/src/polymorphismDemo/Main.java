562d2552eebadf717583db63c811e68b1de46926
Emre-Kurt <yemrek.kurt@outlook.com>
2022-10-13 14:53:26 +0300

    PolymorphismDemo

---
+++ PolymorphismDemo/src/polymorphismDemo/Main.java
@@ -0,0 +1,11 @@
+package polymorphismDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		CustomerManager customerManager =new CustomerManager (new DatabaseLogger());
+		customerManager.add();
+	}
+
+}
